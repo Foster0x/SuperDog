5c502bac9b8ed22fdb750e7adaa86a3b9a415ad0
Foster0x <sfoster2010.sf@gmail.com>
2022-04-13 12:45:36 -0400

    script and js

--- js/site.js
+++ js/site.js
@@ -1,36 +1,85 @@
-// Sep One - controller/accepts requests
-function getMessage() {
-    let msg = document.getElementById("txtMessage").value;
+// Global variable
+const events = [{
+        event: "ComicCon",
+        city: "New York",
+        state: "New York",
+        attendance: 240000,
+        date: "06/01/2017",
+    },
+    {
+        event: "ComicCon",
+        city: "New York",
+        state: "New York",
+        attendance: 250000,
+        date: "06/01/2018",
+    },
+    {
+        event: "ComicCon",
+        city: "New York",
+        state: "New York",
+        attendance: 257000,
+        date: "06/01/2019",
+    },
+    {
+        event: "ComicCon",
+        city: "San Diego",
+        state: "California",
+        attendance: 130000,
+        date: "06/01/2017",
+    },
+    {
+        event: "ComicCon",
+        city: "San Diego",
+        state: "California",
+        attendance: 140000,
+        date: "06/01/2018",
+    },
+    {
+        event: "ComicCon",
+        city: "San Diego",
+        state: "California",
+        attendance: 150000,
+        date: "06/01/2019",
+    },
+    {
+        event: "HeroesCon",
+        city: "Charlotte",
+        state: "North Carolina",
+        attendance: 40000,
+        date: "06/01/2017",
+    },
+    {
+        event: "HeroesCon",
+        city: "Charlotte",
+        state: "North Carolina",
+        attendance: 45000,
+        date: "06/01/2018",
+    },
+    {
+        event: "HeroesCon",
+        city: "Charlotte",
+        state: "North Carolina",
+        attendance: 50000,
+        date: "06/01/2019",
+    },
+];
 
-    // Check for empty string
-    if (msg == '') {
-        alert("You must enter a string!");
-    } else {
-        msg = msg.toLowerCase();
-        let revMessage = reverseMessageA(msg);
-        displayMessage(revMessage);
-    }
-}
+// Builds the dropdown with a list of unique cities
+function buildDropDown() {
 
-// Business Logic Layer
-function reverseMessageA(msg) {
-    let revMessage = "";
-    // Decrimenting loop
-    for (let index = msg.length - 1; index >= 0; index--) {
-        let char = msg[index];
-        revMessage += char;
-    }
-    return revMessage;
-}
+    // First step is to get a handle on the dropdown
+    let eventDD = document.getElementById("eventDropDownList");
+    // Reset the list
+    eventDD.innerHTML = "";
 
-function reverseMessageB(msg) {
-    let revMessage = "";
-    revMessage = msg.split("").reverse().join("");
-    return revMessage;
-}
+    //<li><a class="dropdown-item" href="#"></a></li>
 
-// Final Step - View
-function displayMessage(message) {
-    element = document.getElementById("results");
-    element.innerHTML = message;
+    // Get the template
+    let ddTemplate = document.getElementById("dropDown-template");
+
+    // Grab the template node
+    let ddItem = document.importNode(ddTemplate.content, true);
+
+    
+
 }
